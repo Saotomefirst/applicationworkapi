840ef81fc8b9a81daa7d5cdee8eb56972726bd38
Saotomefirst <jorge.appleseed@gmail.com>
2022-01-03 00:52:48 -0300

    App concluído - Site fora do ar
    O app foi concluído com sucesso, mas o site está fora do ar...
    [ https://uniqueandrocode.000webhostapp.com/hiren/androidtutorial/mycart/getdata.php ]

--- app/src/main/java/com/saotome/applicationworkapi/MainActivity.kt
+++ app/src/main/java/com/saotome/applicationworkapi/MainActivity.kt
@@ -2,7 +2,14 @@ package com.saotome.applicationworkapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.saotome.applicationworkapi.api.MyRetrofit
+import com.saotome.applicationworkapi.model.Product
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +20,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerProduts = findViewById(R.id.recycler_products)
+        recyclerProduts.layoutManager = LinearLayoutManager(this)
+        getData()
+    }
 
+    private fun getData () {
+        val call : Call<List<Product>> =
+            MyRetrofit.instance?.productApi()?.getProductApi() as Call<List<Product>>
+        call.enqueue(object: Callback<List<Product>>{
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                val adapter = ProductAdapter(this@MainActivity, response.body()?.toList() as List<Product>)
+                recyclerProduts.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
